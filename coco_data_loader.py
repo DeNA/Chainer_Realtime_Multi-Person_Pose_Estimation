6c8aafa4f68a08cdce435e176134e84372b3b303
Naoki Kato <naoki.kato@o-09119-mac.local>
2018-02-08 10:33:45 +0900

    fix a bug

--- coco_data_loader.py
+++ coco_data_loader.py
@@ -113,7 +113,7 @@ class CocoDataLoader(DatasetMixin):
         R[0, 2] += bbox[0] / 2 - center[0]
         R[1, 2] += bbox[1] / 2 - center[1]
         rotate_img = cv2.warpAffine(img, R, (int(bbox[0]+0.5), int(bbox[1]+0.5)), flags=cv2.INTER_CUBIC,
-                                    borderMode=cv2.BORDER_CONSTANT, borderValue=127.5)
+                                    borderMode=cv2.BORDER_CONSTANT, borderValue=[127.5, 127.5, 127.5])
         rotate_mask = cv2.warpAffine(mask.astype('uint8')*255, R, (int(bbox[0]+0.5), int(bbox[1]+0.5))) > 0
 
         tmp_poses = np.ones_like(poses)
